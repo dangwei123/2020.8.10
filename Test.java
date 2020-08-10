53c1927a73f58fb353fd71cf3b73c7c1357789fc
党威 <1357711677@qq.com>
2020-08-10 12:51:18 +0800

    Test.java

---
+++ Test.java
@@ -0,0 +1,60 @@
+给定一个字符串 s，计算具有相同数量0和1的非空(连续)子字符串的数量，并且这些子字符串中的所有0和所有1都是组合在一起的。
+
+重复出现的子串要计算它们出现的次数。
+
+示例 1 :
+
+输入: "00110011"
+输出: 6
+解释: 有6个子串具有相同数量的连续1和0：“0011”，“01”，“1100”，“10”，“0011” 和 “01”。
+
+请注意，一些重复出现的子串要计算它们出现的次数。
+
+另外，“00110011”不是有效的子串，因为所有的0（和1）没有组合在一起。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/count-binary-substrings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+方法一：(超时)
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ret=0;
+        for(int i=0;i<s.length();i++){
+            int j=i+1;
+            while(j<s.length()&&s.charAt(j)==s.charAt(i)){
+                j++;
+            }
+            int num=j-i;
+            while(j<s.length()&&s.charAt(j)!=s.charAt(i)&&num>0){
+                j++;
+                num--;
+            }
+            if(num==0) ret++;
+        }
+        return ret;
+    }
+}
+
+
+方法二：
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ret=0;
+        int pre=0;
+        int cur=0;
+        while(cur<s.length()){
+            char c=s.charAt(cur);
+            int num=0;
+            while(cur<s.length()&&s.charAt(cur)==c){
+                cur++;
+                num++;
+            }
+            ret+=Math.min(pre,num);
+            pre=num;
+        }
+        return ret;
+    }
+}
